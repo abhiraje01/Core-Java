e7b5de17a22be5956d11268acfc8f3fec909c2d3
Abhiraje Nimbalkar <158149163+abhiraje01@users.noreply.github.com>
2025-02-14 22:01:19 +0530

    Update Count_even_odd_digit.java

--- Count_even_odd_digit.java
+++ Count_even_odd_digit.java
@@ -1,15 +1,15 @@
 class Count_even_odd_digit
   {
-    public static void main(String[] args)
+    public static void main(String[] args)      //main Method
     {
-      int num=6534;
-      int counteven=0;
-      int countodd=0;
+      int num=6534;            //Declaring a Num Varaible for Storing a Number
+      int counteven=0;         //Declaring a counteven Variable for storing a even count
+      int countodd=0;          //Decalring a countodd  Variable for storing a odd Count 
 
-    while (num>0)
+    while (num>0)                //Using While loop For Iteration
       {
-        int rem=num%10;
-        if (rem%2==0)
+        int rem=num%10;           //Taking Last Digit
+        if (rem%2==0)             //Checking the digit is even or odd
         {
           counteven++;
         }
