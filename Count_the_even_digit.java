446f295fd9f6893656b1c6974af2fc4e3c6828b6
Abhiraje Nimbalkar <158149163+abhiraje01@users.noreply.github.com>
2025-02-14 22:02:53 +0530

    Update Count_the_even_digit.java

--- Count_the_even_digit.java
+++ Count_the_even_digit.java
@@ -1,13 +1,13 @@
 class Count_the_even_digit
   {
-    public static void main(String[] args)
+    public static void main(String[] args)            //Main Method
     {
       int num=23456;  //declare a num variable to store a number
       int count=0;    //declare a count variable to store a count
-      while (num>0)
+      while (num>0)          //Using While Loop
         {
-          int rem=num%10;
-          if (rem%2==0)
+          int rem=num%10;             //Getting Last Digit
+          if (rem%2==0)             //Checking if it is even then increase the count by using if loop
           {
             count++;
           }
