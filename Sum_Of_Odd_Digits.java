d450006dbf29315675710c40a261eff13aed3ee7
Abhiraje Nimbalkar <158149163+abhiraje01@users.noreply.github.com>
2025-02-15 11:45:53 +0530

    Update Sum_Of_Odd_Digits.java

--- Sum_Of_Odd_Digits.java
+++ Sum_Of_Odd_Digits.java
@@ -1,14 +1,14 @@
 class Sum_Of_Odd_Digit
   {
-    public static void main(String[] args)
+    public static void main(String[] args)     //main Method
     {
       int num=2345;   //Variable Declaration and Initialization
-      int sum=0;
+      int sum=0;       //varible declaration and initialization
 
-    while (num>0)
+    while (num>0)         //While Loop
       {
-        int rem=num%10;
-        if (rem%2!=0)
+        int rem=num%10;      //Getting Last Digit
+        if (rem%2!=0)         //Checking last digit is odd or not 
         {
           sum=sum+rem;
         }
