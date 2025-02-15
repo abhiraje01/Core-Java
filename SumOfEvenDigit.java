854426568f6de79606b9f5d1c04e54b5cbd8e126
Abhiraje Nimbalkar <158149163+abhiraje01@users.noreply.github.com>
2025-02-15 11:47:21 +0530

    Update SumOfEvenDigit.java

--- SumOfEvenDigit.java
+++ SumOfEvenDigit.java
@@ -1,11 +1,11 @@
 class Sum_Of_Even_Digit 
 {
-	public static void main(String[] args) 
+	public static void main(String[] args)        //Main Method
 	{
-		int num=2345;
-		int sum=0;
+		int num=2345;              //declaring num variable and initializing
+		int sum=0;                 //declaring and initializing sum variable
 
-		while (num>0)
+		while (num>0)             //using while loop
 		{
 			int rem=num%10;
 			if (rem%2==0)
