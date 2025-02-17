9aa75be1b43be770865afba2a62f3bb3d918eaf9
Abhiraje Nimbalkar <158149163+abhiraje01@users.noreply.github.com>
2025-02-17 23:16:06 +0530

    Update SumOfEachDigit.java

--- SumOfEachDigit.java
+++ SumOfEachDigit.java
@@ -1,17 +1,17 @@
-class SumOfEachDigit 
+class SumOfEachDigit                      //Created a Class
 {
-	public static void main(String[] args) 
+	public static void main(String[] args)         //Main Method
 	{
-		int num=2345;
-		int sum=0;
+		int num=2345;               //Declaring and Initializing Num Varible 
+		int sum=0;               //Declaring and Initializing a sum variable
 
-		while (num>0)
+		while (num>0)                 //using While Lopp
 		{
-			int rem=num%10;
-			sum=sum+rem;
-			num/=10;
+			int rem=num%10;               //taking last digit from Given Number
+			sum=sum+rem;            //Doing Addition of sum(initially Zero)+rem(last Digit)
+			num/=10;            //Deleting Last Digit
 		}
-		System.out.println(sum);
+		System.out.println(sum);         //printing the sum
 		
 	}
 }
