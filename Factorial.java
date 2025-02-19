46829d2bd89ed7f715d26cf670b3cdb22c669a37
Abhiraje Nimbalkar <158149163+abhiraje01@users.noreply.github.com>
2025-02-19 22:24:15 +0530

    Update Factorial.java

--- Factorial.java
+++ Factorial.java
@@ -1,13 +1,16 @@
-class Factorial 
+class Factorial        //Creating a class
 {
-  public static void main(String[] args)
+  public static void main(String[] args)           //main Method
   {
-    int num=5;
-    int fact=1;
-    for(int i=num;i>=1;i--)
+    int num=5;              //declaring and initializing num variable
+    int fact=1;             //declaring and initializing fact variable
+    for(int i=num;i>=1;i--)                 //for loop
       {
-        fact=fact*i;
+        fact=fact*i;            //multiplying fact by i
       }
-    System.out.println(fact);
+    System.out.println(fact);         //printing the fact
   }
 }
+
+
+//output=120
