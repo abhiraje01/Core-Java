6066af1d2ebc4d11cd7c18074c035334b23c29e6
Abhiraje Nimbalkar <158149163+abhiraje01@users.noreply.github.com>
2025-02-20 22:07:05 +0530

    Update Count_the_odd_digit.java

--- Count_the_odd_digit.java
+++ Count_the_odd_digit.java
@@ -1,18 +1,18 @@
-class Count_the_odd_digit
+class Count_the_odd_digit         //creating a class
   {
-    public static void main(String[] args)
+    public static void main(String[] args)               //main Method
     {
-      int num=234567;
-      int count=0;
-      while (num>0)
+      int num=234567;               //decalring and initializing num variable 
+      int count=0;                 //declaring and initializing count variable
+      while (num>0)              //while loop
         {
-          int rem=num%10;
-          if(rem%2!=0)
+          int rem=num%10;                //Taking last Digit
+          if(rem%2!=0)                //checking the odd digit
           {
-            count++;
+            count++;                //if digit is odd then count it
           }
-            num/=10;
+            num/=10;              //deleting the last digit
         }
-      System.out.println(count);
+      System.out.println(count);             //printing the total count
     }
   }
