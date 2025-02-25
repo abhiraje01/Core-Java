c6969cd24ef24818f8639d21b46e962bbbe08a77
Abhiraje Nimbalkar <158149163+abhiraje01@users.noreply.github.com>
2025-02-25 21:44:29 +0530

    Update NeonNo.java

--- NeonNo.java
+++ NeonNo.java
@@ -1,19 +1,19 @@
-class NeonNo
+class NeonNo      //creating a class
 {
-	public static void main(String[] args) 
+	public static void main(String[] args)              //main method
 	{
-		int num =9;
-		int square = num*num;
-		int sum =0;
+		int num =9;                      //declaring and initializing num variable
+		int square = num*num;                       //multiplying num by num
+		int sum =0;                    //declaring and initializing sum variable
 
-		while (square>0)
+		while (square>0)            //while loop
 		{
-			int rem=square%10;
-			sum=sum+rem;
-            square=square/10;
+			int rem=square%10;             //getting last digit
+			sum=sum+rem;                //adding sum and last digit
+            square=square/10;                    //deleting last digit
 			
 		}
-		if (sum==num)
+		if (sum==num)              
 		{
 			System.out.println("No. Is Neon");
 		}
