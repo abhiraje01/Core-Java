d4d27248c08f64292b7c6263eae575dd740126e3
Abhiraje Nimbalkar <158149163+abhiraje01@users.noreply.github.com>
2025-02-25 21:58:51 +0530

    Update Polindrome.java

--- Polindrome.java
+++ Polindrome.java
@@ -1,12 +1,12 @@
-class Polindrome 
+class Polindrome            //creating a class
 {
-	public static void main(String[] args) 
+	public static void main(String[] args)          //main method
 	{
-		int num=1861;
-		int rev=0;
-		int num1=num;
+		int num=1861;                          //declaring and initializing num variable
+		int rev=0;                             //declaring and initializing rev variable
+		int num1=num;                            //declaring and initializing num1 variable
 
-		while (num>0)
+		while (num>0)                        //while loop
 		{
 			int rem=num%10;
 			rev=rev*10+rem;
