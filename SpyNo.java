06b2d7d285c5f884fc9b515a2013c67c170e98f3
Abhiraje Nimbalkar <158149163+abhiraje01@users.noreply.github.com>
2025-02-26 22:09:25 +0530

    Update SpyNo.java

--- SpyNo.java
+++ SpyNo.java
@@ -1,22 +1,22 @@
-class SpyNo 
+class SpyNo              //creating a class
 {
-	public static void main(String[] args) 
+	public static void main(String[] args)            //main method
 	{
 		int num=1234;
-		int sum=0;   //to store the sum value
-		int mul=1;  //to store the mul value
-		int num1 = num;  //This Is for print the statement
+		int sum=0;   //declaring and initializing sum variable
+		int mul=1;  //declaring and initializing mul varaible
+		int num1 = num;  //declaring and initializing num1 variable
 
 		while (num>0)
 		{
-			int rem=num%10;    //to get the last digit
+			int rem=num%10;    //getting the last digit
 			mul=mul*rem;
 			sum=sum+rem;
-			num=num/10;        //to delete the last digit
+			num=num/10;        //deleting the last digit
 		}
 		if (sum==mul)
 		{
-			System.out.println(num1 +" Is Spy No.");
+			System.out.println(num1 +" Is Spy No.");                //printing if it is spy no
 		}
 		else 
 			System.out.println(num1 + " Is Not a Spy No.");
