307d0eaf35aabced4817629d86bea69bf5c70e2f
Abhiraje Nimbalkar <158149163+abhiraje01@users.noreply.github.com>
2025-03-03 21:56:56 +0530

    Update AutomorphicNo.java

--- AutomorphicNo.java
+++ AutomorphicNo.java
@@ -1,23 +1,23 @@
-class AutomorphicNo 
+class AutomorphicNo           //creating a class
 {
-	public static void main(String[] args) 
+	public static void main(String[] args)           //main method
 	{
-		int num=76;
-		int count=0;
-		int res=num;
+		int num=76;                  //declaring and initializing a num variable
+		int count=0;                  //declaring and initializing count variable
+		int res=num;                  //declaring and initializing res variable
 
 		while (num>0)
 		{
-			count++;
-			num/=10;
+			count++;                  //increasing the count
+			num/=10;                //deleting the last digit
 		}
 		num=res;
-		int square=num*num;
-		int den=1;
+		int square=num*num;                    //Performing mul operation
+		int den=1;                         //declaring and initializing den variable
 		for (int i=1;i<=count;i++ )
 		{
-			den=den*10;
-		}
+			den=den*10;               //multiplying by 10
+		} 
 		int rem=square%den;
 		System.out.println(rem);
 	}
