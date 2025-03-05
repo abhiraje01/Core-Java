eb8844914e04c7e26fb258bcbe23c93dc2ecd680
Abhiraje Nimbalkar <158149163+abhiraje01@users.noreply.github.com>
2025-03-05 21:31:39 +0530

    Add files via upload
    To check Given No. Is Strontio No. or Not

---
+++ StrontioNo.java
@@ -0,0 +1,51 @@
+class StrontioNo 
+{
+	public static void main(String[] args) 
+	{
+		int num=1386;
+		int count=0;
+		int res=num;
+		while (num>0)
+		{
+			count++;
+			num/=10;
+		}
+		if (count==4)
+		{
+			num=res;
+			int mul=num*2;
+			int den=1;
+			int tensdigit=0;
+			int hundreddigit=0;
+			System.out.println("mul:"+ mul);
+			while (mul>0)
+			{
+				den=den*10;
+				int rem=mul%10;
+				if (den==100)
+				{
+					System.out.println("tens" + rem);
+					tensdigit=rem;
+				}
+				if (den==1000)
+				{
+					System.out.println("hundreds" + rem);
+					hundreddigit=rem;
+				}
+				mul/=10;
+			}
+			if (tensdigit==hundreddigit)
+			{
+				System.out.println(" It Is Strontio No.");
+			}
+			else 
+			{
+				System.out.println("It is Not a Strontio No.");
+			}
+		}
+		else 
+		{
+			System.out.println("It Is Not a Strontio No.");
+		}
+	}
+}
