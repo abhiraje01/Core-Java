50a0c777262f5b322a053cbdefb2bff8d2cb7b7d
Abhiraje Nimbalkar <158149163+abhiraje01@users.noreply.github.com>
2025-03-09 10:11:15 +0530

    Update BuzzNo.java

--- BuzzNo.java
+++ BuzzNo.java
@@ -1,12 +1,12 @@
-class BuzzNo 
+class BuzzNo                                 //Creating a class
 {
-	public static void main(String[] args) 
+	public static void main(String[] args)         //main method
 	{
-		int num=45;
-		int rem=num%10;
-		if (rem==7 || num%7==0)
+		int num=45;                        //Declaring and Initializing a num Variable
+		int rem=num%10;                   //Getting Last Digit
+		if (rem==7 || num%7==0)              //Checking the Condition
 		{
-			System.out.println(num+ " Is a Buzz No.");
+			System.out.println(num+ " Is a Buzz No.");             //Printing
 		}
 		else 
 			System.out.println(num+ " Is Not a Buzz No.");
