6c8cbb45c04800c3cf42eab7f08e7c6036a13952
Abhiraje Nimbalkar <158149163+abhiraje01@users.noreply.github.com>
2025-03-11 18:53:20 +0530

    Update PerfectNo.java

--- PerfectNo.java
+++ PerfectNo.java
@@ -1,20 +1,20 @@
-class PerfectNo 
+class PerfectNo                       //creating a class
 {
-	public static void main(String[] args) 
+	public static void main(String[] args)                //main method
 	{
-		int num=28;
-		int sum=0;
+		int num=28;             //declaring and initializing num variable
+		int sum=0;             //declaring and initializing sum variable
 
-		for (int i=1;i<num;i++ )
+		for (int i=1;i<num;i++ )                  //for loop
 		{
-			if (num%i==0)
+			if (num%i==0)                    //checking the condition
 			{
-				sum=sum+i;
+				sum=sum+i;                  //performing operation
 			}
 		}
 		if (sum==num)
-		{
-			System.out.println(num+ " Is Perfect No.");
+		{       
+			System.out.println(num+ " Is Perfect No.");                  //printing
 		}
 		else
             System.out.println(num+ " Is Not a Perfect No");
