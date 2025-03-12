00c9b4240cb5f0232761b6fa4be84af414f56ec4
Abhiraje Nimbalkar <158149163+abhiraje01@users.noreply.github.com>
2025-03-12 16:58:15 +0530

    Update DecimalToBinary.java

--- DecimalToBinary.java
+++ DecimalToBinary.java
@@ -1,19 +1,19 @@
-class DecimalToBinary 
+class DecimalToBinary                           //Creating a class
 {
-	public static void main(String[] args) 
+	public static void main(String[] args)                   //main Method
 	{
-		int num=12;
-		int bin=0;
-		int i=1;
+		int num=12;                             //Declaring and Iniializing num Variable
+		int bin=0;                            //Declaring and Initializing bin Variable
+		int i=1;                            //Declaring and Initializing i Variable
 
 		while (num>0)
 		{
-			int rem=num%2;
-			bin=rem*i+bin;
-			num/=2;
+			int rem=num%2;                   //Modulus of 2
+			bin=rem*i+bin;                       //Operations
+			num/=2;                           //Division of 2
 			i*=10;
 
 		}
-		System.out.println(bin);
+		System.out.println(bin);               //Printing the Result
 	}
 }
