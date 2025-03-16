74e936f3d856677023bec061a770fffb3302db8d
Abhiraje Nimbalkar <158149163+abhiraje01@users.noreply.github.com>
2025-03-16 23:22:46 +0530

    Update DuckNo.java

--- DuckNo.java
+++ DuckNo.java
@@ -1,35 +1,35 @@
-class DuckNo 
+class DuckNo                                       //creating a class
 {
-	public static void main(String[] args) 
+	public static void main(String[] args)                          //main method
 	{
-		int num=035;
-		int count=0;
-		int res=num;
-		int rev=0;
+		int num=035;                              //declaring and initializing num variable
+		int count=0;                         //declaring and initializing count variable 
+		int res=num;                     //declaring and initializing res variable
+		int rev=0;                        //declaring and initializing rev variable
 
 		while (num>0)
 		{
-			int rem=num%10;
+			int rem=num%10;                       //getting last digit
 			if (rem==0)
 			{
-				count++;
+				count++;                    //increasing count by 1
 			}
-			num/=10;
+			num/=10;                     //deleting the last digit
 		}
-		num=res;
+		num=res;                              //reinitializing the num variable 
 		while (num>0)
 		{
-			int rem=num%10;
-			rev=rev*10+rem;
-			num/=10;
-		}
-		int rem1=rev%10;
-		if (rem1!=0 && count>=1)
+			int rem=num%10;                      //deleting the last digit
+			rev=rev*10+rem;                       //performing the operation
+			num/=10;                    //deleting the last digit
+		} 
+		int rem1=rev%10;                   //getting last digit of rev value
+		if (rem1!=0 && count>=1)                      //checking the condition
 		{
-			System.out.println(" It iS Duck No.");
+			System.out.println(" It iS Duck No.");                      //printing it if conition matches
 		}
 		else
-			System.out.println(" It is Not a Duck No.");
+			System.out.println(" It is Not a Duck No.");                     //printing statement if condition not matches
 
 	}
 }
