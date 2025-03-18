b74f4d01c75719bbdcf9f31c7f55fb21fcd6c6b0
Abhiraje Nimbalkar <158149163+abhiraje01@users.noreply.github.com>
2025-03-18 23:09:20 +0530

    Update Count_the_Digit.java

--- Count_the_Digit.java
+++ Count_the_Digit.java
@@ -1,7 +1,7 @@
 class Count_the_Digit
   {
-    public static void main(String[] args)         //Main() 
-    {
+    public static void main(String[] args)         //main()
+  
       int num=23456;     // declaring num variable to storing the number
       int count=0;    // Declaring count variable To Storing the count
       while (num>0)
@@ -9,7 +9,7 @@ class Count_the_Digit
           count++;            //Increment the count by 1
           num/=10;             //Deleting the last digit
         }
-      System.out.println(count);         // Printing The fina count
+      System.out.println(count);         // Printing The final count
     }
   }
 
