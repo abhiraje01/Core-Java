1f713660ad638234d391e634fc26f05aa634656b
Abhiraje Nimbalkar <158149163+abhiraje01@users.noreply.github.com>
2025-03-23 23:54:28 +0530

    Create cube_no_series.java

---
+++ cube_no_series.java
@@ -0,0 +1,10 @@
+public class CubeSeries {
+    public static void main(String[] args) {
+        int n = 10;
+        
+        System.out.print("Cube Number Series: ");
+        for (int i = 1; i <= n; i++) {
+            System.out.print((i * i * i) + " ");
+        }
+    }
+}
