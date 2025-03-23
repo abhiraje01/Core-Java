8db01b9c7255a9348e6c8b230814e0d89d7be1c7
Abhiraje Nimbalkar <158149163+abhiraje01@users.noreply.github.com>
2025-03-23 23:53:44 +0530

    Create Square_Number_Series.java

---
+++ Square_Number_Series.java
@@ -0,0 +1,10 @@
+public class SquareSeries {
+    public static void main(String[] args) {
+        int n = 10;
+        
+        System.out.print("Square Number Series: ");
+        for (int i = 1; i <= n; i++) {
+            System.out.print((i * i) + " ");
+        }
+    }
+}
