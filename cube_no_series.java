67c5492fec9a717db20dd5582db85a5cac2b70e7
Abhiraje Nimbalkar <158149163+abhiraje01@users.noreply.github.com>
2025-03-24 10:37:05 +0530

    Update cube_no_series.java

--- cube_no_series.java
+++ cube_no_series.java
@@ -1,8 +1,10 @@
-public class CubeSeries {
-    public static void main(String[] args) {
-        int n = 10;
+public class CubeSeries                    //creating a class
+{                            
+    public static void main(String[] args)               //main method
+    {
+        int n = 10;                 //declaring and initializing n variable
         
-        System.out.print("Cube Number Series: ");
+        System.out.print("Cube Number Series: ");                    //printing the number
         for (int i = 1; i <= n; i++) {
             System.out.print((i * i * i) + " ");
         }
