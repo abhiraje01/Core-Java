bf4b9c28fb7ab2953ce6cb6c4960ab888668ae69
Abhiraje Nimbalkar <158149163+abhiraje01@users.noreply.github.com>
2025-03-25 21:46:56 +0530

    Update Generate_random_no.java

--- Generate_random_no.java
+++ Generate_random_no.java
@@ -1,8 +1,10 @@
-public class RandomNumber {
-    public static void main(String[] args) {
-        int min = 1, max = 100;
+public class RandomNumber                       //creating a class
+    {
+    public static void main(String[] args)              //main method
+        {
+        int min = 1, max = 100;                  //declaring and initializing min variable
         int randomNum = (int)(Math.random() * (max - min + 1)) + min;
 
-        System.out.println("Random Number: " + randomNum);
+        System.out.println("Random Number: " + randomNum);           //printing the statement
     }
 }
