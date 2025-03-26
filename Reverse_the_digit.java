3929989ab5443e680cd418fb47556a624b32e370
Abhiraje Nimbalkar <158149163+abhiraje01@users.noreply.github.com>
2025-03-26 19:07:55 +0530

    Update Reverse_the_digit.java

--- Reverse_the_digit.java
+++ Reverse_the_digit.java
@@ -1,15 +1,15 @@
-class Reverse_the_digit
+class Reverse_the_digit                //creating a class
   {
-    public static void main(String[] args)
+    public static void main(String[] args)        //main method
     {
-      int num=1234;
-      int rev=0;
-
+      int num=1234;                    //declaring and initializing num variable
+      int rev=0;                      //declaring and initializing rev variable
+ 
     while (num>0)
       {
-        int rem=num%10;
-        rev=rev*10+rem;
-        num/=10;
+        int rem=num%10;                     //getting last digit
+        rev=rev*10+rem;                    //performing operations
+        num/=10;                           //deleting the last digit 
       }
       System.out.println(rev);
     }
