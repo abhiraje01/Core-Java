0b5485c501d022ef7c98e5c5aafd2e92a9991c38
Abhiraje Nimbalkar <158149163+abhiraje01@users.noreply.github.com>
2025-03-27 23:27:32 +0530

    Update first_prime_n_no.java

--- first_prime_n_no.java
+++ first_prime_n_no.java
@@ -1,9 +1,9 @@
-public class PrimeSeries {
-    public static void main(String[] args) {
-        int n = 10, count = 0, num = 2;
+public class PrimeSeries {                                          //creating a class
+    public static void main(String[] args) {                                 //main method
+        int n = 10, count = 0, num = 2;                                   //declaring and initializing a variables
 
         System.out.print("First " + n + " Prime Numbers: ");
-        while (count < n) {
+        while (count < n) {                                                     //using while loop
             if (isPrime(num)) {
                 System.out.print(num + " ");
                 count++;
@@ -12,7 +12,7 @@ public class PrimeSeries {
         }
     }
 
-    public static boolean isPrime(int num) {
+    public static boolean isPrime(int num) {                                 //isprime method
         if (num < 2) return false;
         for (int i = 2; i <= Math.sqrt(num); i++) {
             if (num % i == 0) return false;
