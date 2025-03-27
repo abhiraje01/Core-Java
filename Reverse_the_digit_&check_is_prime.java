a47f7899455d871f1f2009ede13a6df85731ffcc
Abhiraje Nimbalkar <158149163+abhiraje01@users.noreply.github.com>
2025-03-27 23:26:04 +0530

    Update Reverse_the_digit_&check_is_prime.java

--- Reverse_the_digit_&check_is_prime.java
+++ Reverse_the_digit_&check_is_prime.java
@@ -1,10 +1,10 @@
-public class ReversePrime {
-    public static void main(String[] args) {
-        int num = 37, reversed = 0, temp = num;
+public class ReversePrime {                            //craeting a class
+    public static void main(String[] args) {              //main Method
+        int num = 37, reversed = 0, temp = num;                //declaring and initializing a variable
 
-        while (temp > 0) {
-            reversed = reversed * 10 + temp % 10;
-            temp /= 10;
+        while (temp > 0) {                       //while loop
+            reversed = reversed * 10 + temp % 10;               //performing operations
+            temp /= 10;                          //deleting last digit
         }
 
         if (isPrime(reversed))
@@ -13,7 +13,7 @@ public class ReversePrime {
             System.out.println(reversed + " is NOT Prime");
     }
 
-    public static boolean isPrime(int n) {
+    public static boolean isPrime(int n) {                   //isprime method
         if (n < 2) return false;
         for (int i = 2; i <= Math.sqrt(n); i++) {
             if (n % i == 0) return false;
