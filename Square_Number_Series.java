0eb90dba19dbfd57ccc913208b9c141889ae0c0f
Abhiraje Nimbalkar <158149163+abhiraje01@users.noreply.github.com>
2025-03-28 23:39:44 +0530

    Update Square_Number_Series.java

--- Square_Number_Series.java
+++ Square_Number_Series.java
@@ -1,9 +1,9 @@
-public class SquareSeries {
-    public static void main(String[] args) {
-        int n = 10;
+public class SquareSeries {                      //creating a class
+    public static void main(String[] args) {                //main method
+        int n = 10;                                        //declaring and initializing n variable
         
-        System.out.print("Square Number Series: ");
-        for (int i = 1; i <= n; i++) {
+        System.out.print("Square Number Series: ");                              //printing the statement
+        for (int i = 1; i <= n; i++) {                                             //using for loop
             System.out.print((i * i) + " ");
         }
     }
