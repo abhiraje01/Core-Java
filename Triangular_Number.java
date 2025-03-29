d9a94a015eac35608bd14469c4bb45fcfcec0e82
Abhiraje Nimbalkar <158149163+abhiraje01@users.noreply.github.com>
2025-03-29 22:34:14 +0530

    Update Triangular_Number.java

--- Triangular_Number.java
+++ Triangular_Number.java
@@ -1,11 +1,11 @@
-public class TriangularSeries {
-    public static void main(String[] args) {
-        int n = 10;
+public class TriangularSeries {                    //craeting a class
+    public static void main(String[] args) {              //main method
+        int n = 10;                        //declaring and initializing n variable
         
-        System.out.print("Triangular Number Series: ");
-        for (int i = 1; i <= n; i++) {
-            int triNum = (i * (i + 1)) / 2;
-            System.out.print(triNum + " ");
+        System.out.print("Triangular Number Series: ");               //printing the statement
+        for (int i = 1; i <= n; i++) {                   //using for loop
+            int triNum = (i * (i + 1)) / 2;                       //performing operations
+            System.out.print(triNum + " ");                              //printing the statement
         }
     }
 }
