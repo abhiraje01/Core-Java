b4956376e516ba93dd3fa512aba3329edeeb520e
Abhiraje Nimbalkar <158149163+abhiraje01@users.noreply.github.com>
2025-03-31 23:11:49 +0530

    Update print_alternative_digit.java

--- print_alternative_digit.java
+++ print_alternative_digit.java
@@ -1,16 +1,16 @@
 class printalternative_digit                     //creating a class
   { 
-    public static void main(String[] args)                     //main method
+    public static void main(String[] args)                     //main() method
     { 
       int num=8790;                      //declaring and initializing num variable
-      int count=0;                        //declaring and initialzing count variable 
+      int count=0;                        //declaring and initialzing count variable
       while (num>0)
         {
           int rem=num%10;                          //getting last digit
           count++;                                   //increasing the count by 1
           if (count%2==0)
           {
-            Sytem.out.println(rem);
+            Sytem.out.println(rem);                        //printing the statement
           }
           num/=10;                                       //deleting the last digit
         }
