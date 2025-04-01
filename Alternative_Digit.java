ce41296729da40844590a31766edb5cf01c7f992
Abhiraje Nimbalkar <158149163+abhiraje01@users.noreply.github.com>
2025-04-01 22:14:03 +0530

    Update Alternative_Digit.java

--- Alternative_Digit.java
+++ Alternative_Digit.java
@@ -5,15 +5,15 @@ class Alternative_Digit                            //Creating a Class
 		int num=42537;                    //Declaring and Initializing num Variable
 		int count=0;                  //Declairng and Initializing Count Variable
 
-		while (num>0)                       //While Loop
+		while (num>0)                       //While loop
 		{
-			int rem=num%10;               //Getting Last Digit
+			int rem=num%10;               //Getting Last digit
 			count++;                       //Using Post Increment for Count
 			if (count%2==0)                  //checking the count dividation
 			{
 				System.out.println(rem);             //Printing the digit
 			}
-			num/=10;                          //Deleting the last Digit
+			num/=10;                          //Deleting the last digit
 
 		}
 	}
