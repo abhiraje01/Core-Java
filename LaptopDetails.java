597808ac542d2f0223a1581cd63a3fb9c3e904af
Abhiraje Nimbalkar <158149163+abhiraje01@users.noreply.github.com>
2025-04-09 23:23:02 +0530

    Add files via upload

---
+++ LaptopDetails.java
@@ -0,0 +1,29 @@
+class LaptopDetails
+{
+	public static void main(String[] args)
+	{
+		String Devicename = "Asus VivoBook14";
+		String Processor = "11th Gen i5 @2.40 2.42GHz";
+		String Ram = "8.00GB";
+		long DeviceId = 2346954794793l;
+		boolean PenAndTouch = false;
+		long ProductId = 75836894749l;
+		int MfgYear = 18;
+		boolean Insurance = true;
+		int Price = 56000;
+
+		System.out.println("========================================================");
+		System.out.println( Devicename +" Details");
+		System.out.println("Laptpop's name is: " + Devicename);
+		System.out.println("Lapotpo's Processor: " + Processor);
+		System.out.println("Laptop's Ram: " + Ram);
+		System.out.println("Laptop's DeviceId: " +DeviceId);
+		System.out.println("Laptop's PenAndTouch  Facility: "+ PenAndTouch);
+		System.out.println("Laptop's ProductId: " +ProductId);
+		System.out.println("Laptop's MfgYear: " +MfgYear);
+		System.out.println("Laptop's Insurance Features: "+Insurance);
+		System.out.println("Lpatop's Price: " +Price);
+		System.out.println("========================================================");
+	}
+		
+}
